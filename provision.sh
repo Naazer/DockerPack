8d7e2bd168affd76cf70bf55de229d82b652abca
rivaros <rivantsiv@gmail.com>
2016-04-30 23:48:39 +0300

    Adapting for Windows

--- provision.sh
+++ provision.sh
@@ -52,107 +52,126 @@ fi
 # Install docker
 if [ -n "$install" ]
 then
-    if [ "$(uname)" == "Darwin" ]; then
-        UNAME_S=`uname -s`
-        UNAME_P=`uname -p`
-        UNAME_M=`uname -m`
 
-        # Install docker-machine
-        echo "Checking docker-machine version..."
-        CURRENT_DOCKER_MACHINE_VERSION=`docker-machine --version`
-        if [[ $CURRENT_DOCKER_MACHINE_VERSION == *$DOCKER_MACHINE_VERSION* ]]
-        then
-            echo "Latest version already installed. Skipping..."
+    UNAME_S=`uname -s`
+    UNAME_P=`uname -p`
+    UNAME_M=`uname -m`
+
+    # Install docker-machine
+    echo "Checking docker-machine version..."
+    CURRENT_DOCKER_MACHINE_VERSION=`docker-machine --version`
+    if [[ $CURRENT_DOCKER_MACHINE_VERSION == *$DOCKER_MACHINE_VERSION* ]]
+    then
+        echo "Latest version already installed. Skipping..."
+    else
+        if [ "$(expr substr $(uname -s) 1 6 2>/dev/null)" == "CYGWIN" ]; then
+            echo "Docker machine of version $DOCKER_MACHINE_VERSION not found. Download latest docker-toolbox for Windows"
+            exit
         else
             sudo curl -L https://github.com/docker/machine/releases/download/v$DOCKER_MACHINE_VERSION/docker-machine-$UNAME_S-$UNAME_M > /usr/local/bin/docker-machine && \
             chmod +x /usr/local/bin/docker-machine
         fi
+    fi
 
-        # Install docker-compose
-        echo "Checking docker-compose version..."
-        CURRENT_DOCKER_COMPOSE_VERSION=`docker-compose --version`
-        if [[ $CURRENT_DOCKER_COMPOSE_VERSION == *$DOCKER_COMPOSE_VERSION* ]]
-        then
-            echo "Latest version already installed. Skipping..."
+    # Install docker-compose
+    echo "Checking docker-compose version..."
+    CURRENT_DOCKER_COMPOSE_VERSION=`docker-compose --version`
+    if [[ $CURRENT_DOCKER_COMPOSE_VERSION == *$DOCKER_COMPOSE_VERSION* ]]
+    then
+        echo "Latest version already installed. Skipping..."
+    else
+        if [ "$(expr substr $(uname -s) 1 6 2>/dev/null)" == "CYGWIN" ]; then
+            echo "Docker-compose of version $DOCKER_COMPOSE_VERSION not found. Download latest docker-toolbox for Windows"
+            exit
         else
             rm -rf /usr/local/bin/docker
             sudo curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$UNAME_S-$UNAME_M > /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
         fi
+    fi
 
-        # Install docker
-        echo "Checking docker version..."
-        CURRENT_DOCKER_VERSION=`docker --version`
-        if [[ $CURRENT_DOCKER_VERSION == *$DOCKER_VERSION* ]]
-        then
-            echo "Latest version already installed. Skipping..."
+    # Install docker
+    echo "Checking docker version..."
+    CURRENT_DOCKER_VERSION=`docker --version`
+    if [[ $CURRENT_DOCKER_VERSION == *$DOCKER_VERSION* ]]
+    then
+        echo "Latest version already installed. Skipping..."
+    else
+        if [ "$(expr substr $(uname -s) 1 6 2>/dev/null)" == "CYGWIN" ]; then
+            echo "Docker of version $DOCKER_VERSION not found. Download latest docker-toolbox for Windows"
+            exit
         else
             sudo curl -L https://get.docker.com/builds/$UNAME_S/$UNAME_M/docker-$DOCKER_VERSION.tgz | tar -zxv -C /usr/local/bin --strip-components 1 > /usr/local/bin/docker
             sudo chmod +x /usr/local/bin/docker
         fi
+    fi
 
-        # Redownkoad iso if necessary
-        echo "Checking ISO checksum..."
-        chmod +x iso/download.sh && iso/download.sh https://github.com/boot2docker/boot2docker/releases/download/v$BOOT2DOCKER_VERSION/boot2docker.iso $B2D_ISO_CHECKSUM
+    # Redownkoad iso if necessary
+    echo "Checking ISO checksum..."
+    chmod +x iso/download.sh && iso/download.sh https://github.com/boot2docker/boot2docker/releases/download/v$BOOT2DOCKER_VERSION/boot2docker.iso $B2D_ISO_CHECKSUM
 
 
-        #Creating docker-machine instance
-        echo "Creating docker-machine '$MACHINE_NAME'..."
-        docker-machine ls -q | grep '^$MACHINE_NAME$' || MACHINE_EXISTS=1
+    #Creating docker-machine instance
+    echo "Creating docker-machine '$MACHINE_NAME'..."
+    docker-machine ls | grep "^$MACHINE_NAME " && MACHINE_EXISTS=1
+    if [ -n "$MACHINE_EXISTS" ]
+    then
         read -r -p "Docker machine with name '$MACHINE_NAME' exists. Recreate? [yes/no]" response
-        if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-        then
-            # Create virtual machine
-            VBoxManage controlvm $MACHINE_NAME poweroff 2>/dev/null || true
-            VBoxManage unregistervm $MACHINE_NAME --delete 2>/dev/null || true
-            docker-machine rm --force $MACHINE_NAME 2>/dev/null || true
-            docker-machine create --driver=virtualbox --virtualbox-memory=$VB_MEMORY --virtualbox-cpu-count=$VB_CPU_C \
-                --virtualbox-boot2docker-url=iso/boot2docker.iso --virtualbox-hostonly-cidr=$VB_NETWORK \
-                --virtualbox-disk-size=$VB_DISK_SIZE \
-                --virtualbox-no-share \
-                $MACHINE_NAME
-        else
-            echo "Assuming 'No'"
-        fi
+    fi
+    if [[ $response =~ ^([yY][eE][sS]|[yY])$ || -z "$MACHINE_EXISTS" ]]
+    then
+        # Create virtual machine
+        VBoxManage controlvm $MACHINE_NAME poweroff 2>/dev/null || true
+        VBoxManage unregistervm $MACHINE_NAME --delete 2>/dev/null || true
+        docker-machine rm --force $MACHINE_NAME 2>/dev/null || true
+        docker-machine create --driver=virtualbox --virtualbox-memory=$VB_MEMORY --virtualbox-cpu-count=$VB_CPU_C \
+            --virtualbox-boot2docker-url=iso/boot2docker.iso --virtualbox-hostonly-cidr=$VB_NETWORK \
+            --virtualbox-disk-size=$VB_DISK_SIZE \
+            --virtualbox-no-share \
+            $MACHINE_NAME
+    else
+        echo "Assuming 'No'"
+    fi
 
-        echo "Provisioning '$MACHINE_NAME'..."
+    echo "Provisioning '$MACHINE_NAME'..."
 
-        docker-machine scp machine/files/bootsync.sh $MACHINE_NAME:/tmp/bootsync.sh
-        docker-machine scp machine/files/aliases.sh $MACHINE_NAME:/tmp/aliases.sh
-        docker-machine scp machine/scripts/provision.sh $MACHINE_NAME:/tmp/provision.sh
-        docker-machine scp ~/.ssh/id_rsa.pub $MACHINE_NAME:/tmp/id_rsa.pub
-        docker-machine ssh $MACHINE_NAME /tmp/provision.sh $DOCKER_COMPOSE_VERSION
+    docker-machine scp machine/files/bootsync.sh $MACHINE_NAME:/tmp/bootsync.sh
+    docker-machine scp machine/files/aliases.sh $MACHINE_NAME:/tmp/aliases.sh
+    docker-machine scp machine/scripts/provision.sh $MACHINE_NAME:/tmp/provision.sh
+    docker-machine scp ~/.ssh/id_rsa.pub $MACHINE_NAME:/tmp/id_rsa.pub
+    docker-machine ssh $MACHINE_NAME /tmp/provision.sh $DOCKER_COMPOSE_VERSION
 
-        #set TLS
-        sed -iE '/192.168.10.10/d' $HOME/.ssh/known_hosts
-        docker-machine regenerate-certs -f $MACHINE_NAME
-        eval $(docker-machine env $MACHINE_NAME)
+    #set TLS
+    sed -iE '/192.168.10.10/d' $HOME/.ssh/known_hosts
+    docker-machine regenerate-certs -f $MACHINE_NAME
+    if [ "$(expr substr $(uname -s) 1 6 2>/dev/null)" == "CYGWIN" ]; then
+        # On Windows we get a strange delay with certificate setup
+        echo "Waiting 2 minutes..."
+        sleep 120
+    fi
+    eval $(docker-machine env $MACHINE_NAME)
 
-        # Detect shell to write to the right .rc file
-        if [[ $SHELL == '/bin/bash' || $SHELL == '/bin/sh' ]]; then SOURCE_FILE=".bash_profile"; fi
-        if [[ $SHELL == '/bin/zsh' ]]; then	SOURCE_FILE=".zshrc"; fi
-        if [[ $SOURCE_FILE ]]; then
-            echo $HOME/$SOURCE_FILE
-            sed -iE '/DOCKER_TLS_VERIFY/d' $HOME/$SOURCE_FILE
-            sed -iE '/DOCKER_HOST/d' $HOME/$SOURCE_FILE
-            sed -iE '/DOCKER_CERT_PATH/d' $HOME/$SOURCE_FILE
-            sed -iE '/DOCKER_MACHINE_NAME/d' $HOME/$SOURCE_FILE
-            sed -iE '/# Run this command to configure your shell:/d' $HOME/$SOURCE_FILE
-            sed -iE '/eval $(docker-machine env/d' $HOME/$SOURCE_FILE
-            docker-machine env $MACHINE_NAME >> $HOME/$SOURCE_FILE
-            mkdir -p ~/scripts && cp machine/files/aliases.sh $HOME/scripts/aliases.sh
-            grep "source ~/scripts/aliases.sh" < $HOME/$SOURCE_FILE > /dev/null 2>&1 || echo 'source ~/scripts/aliases.sh' >> $HOME/$SOURCE_FILE
+    # Detect shell to write to the right .rc file
+    if [[ $SHELL == '/bin/bash' || $SHELL == '/bin/sh' ]]; then SOURCE_FILE=".bash_profile"; fi
+    if [[ $SHELL == '/bin/zsh' ]]; then	SOURCE_FILE=".zshrc"; fi
+    if [[ $SOURCE_FILE ]]; then
+        echo $HOME/$SOURCE_FILE
+        sed -iE "/DOCKER_TLS_VERIFY/d" $HOME/$SOURCE_FILE
+        sed -iE "/DOCKER_HOST/d" $HOME/$SOURCE_FILE
+        sed -iE "/DOCKER_CERT_PATH/d" $HOME/$SOURCE_FILE
+        sed -iE "/DOCKER_MACHINE_NAME/d" $HOME/$SOURCE_FILE
+        sed -iE "/# Run this command to configure your shell:/d" $HOME/$SOURCE_FILE
+        sed -iE "/env $MACHINE_NAME/d" $HOME/$SOURCE_FILE
+        docker-machine env $MACHINE_NAME >> $HOME/$SOURCE_FILE
+        mkdir -p ~/scripts && cp machine/files/aliases.sh $HOME/scripts/aliases.sh
+        grep "source ~/scripts/aliases.sh" < $HOME/$SOURCE_FILE > /dev/null 2>&1 || echo 'source ~/scripts/aliases.sh' >> $HOME/$SOURCE_FILE
 
-        else
-            echo -e "${red}Cannot detect your shell. Please manually add the following to your respective .rc or .profile file:${NC}"
-            echo -e "$DOCKER_HOST_EXPORT"
-        fi
-    elif [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "Linux" ]; then
-        echo "Linux detected"
-    elif [ "$(expr substr $(uname -s) 1 6 2>/dev/null)" == "CYGWIN" ]; then
-        echo "You are on Windows. The only way for now is downloading latest docker toolbox. Go to docker site and do it."
+    else
+        echo -e "${red}Cannot detect your shell. Please manually add the following to your respective .rc or .profile file:${NC}"
+        echo -e "$DOCKER_HOST_EXPORT"
     fi
 fi
+
 
 if [ -n "$buildiso" ]
 then
