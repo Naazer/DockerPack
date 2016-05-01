ac966dd461c43d9aef3203281bb01f5ea3750927
rivaros <rivantsiv@gmail.com>
2016-05-01 08:01:32 +0300

    adjustments

--- machine/files/bootsync.sh
+++ machine/files/bootsync.sh
@@ -11,7 +11,7 @@ sudo su -c "tce-load -i /var/lib/boot2docker/tce/*.tcz" docker
 sudo chmod -R +x /var/lib/boot2docker/bin
 for i in /var/lib/boot2docker/bin/*; do
 	sudo chmod +x $i
-	sudo ln -sf $i /usr/local/bin/$(basename $i)
+	sudo ln -sfn $i /usr/local/bin/$(basename $i)
 done
 
 # Start nfs client utilities
@@ -22,10 +22,10 @@ sudo ifconfig eth1 192.168.10.10 netmask 255.255.255.0 broadcast 192.168.10.255 
 sudo kill -KILL $(cat /var/run/udhcpc.eth1.pid) 2>/dev/null || echo "DHCP already killed"
 
 #Create symlink to scripts folder
-sudo ln -s -f /mnt/sda1/scripts /home/docker/scripts
+sudo ln -sfn /mnt/sda1/scripts /home/docker/scripts
 sudo grep -q "for f in \~/scripts/\*.sh; do source \$f; done" /home/docker/.ashrc || sudo echo "for f in ~/scripts/*.sh; do source \$f; done" >> /home/docker/.ashrc
 
 #Create symlink to projects folder
-sudo ln -s -f /mnt/sda1/projects /projects
+sudo ln -sfn /mnt/sda1/projects /projects
 #Create symlink to certificates folder
-sudo ln -s -f /mnt/sda1/ssl_certs /ssl_certs
+sudo ln -sfn /mnt/sda1/ssl_certs /ssl_certs
